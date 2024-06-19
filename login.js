b0b0565e444ef1425a797e78aad40972c5188b62
di.le@fecredit.com.vn-at-273658691124 <di.le@fecredit.com.vn>
2024-06-19 13:48:32 +0700

    update

--- login.js
+++ login.js
@@ -8,17 +8,17 @@ const app = createApp({
             ForgotPWD: 'https://sso.ubank.vn/forgotPassword?client_id=47rj3rlovbcgdjvm9j5uc9glcj&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://portal.ubank.vn/login',
             logoImg: './shared/images/fecredit.png',
             logoVN: './shared/images/vietnam.png',
-            logoUK: './shared/images/UK.png',
+            logoEN: './shared/images/UK.png',
             lang: 'vi',
             i18n: {
                 'vi': {
-                    'Username': 'Tên tài khoản đăng nhập',
+                    'Username': 'Tài khoản đăng nhập',
                     'Password': 'Mật khẩu',
                     'ForgotPassword': 'Bạn quên mật khẩu?',
-                    'SignUp': 'Tại đây',
-                    'EnterUsername': 'Nhập tài khoản đăng nhập',
+                    'SignUp': 'Đăng ký',
+                    'EnterUsername': 'Nhập tài khoản',
                     'EnterPassword': 'Nhập mật khẩu',
-                    'NeedAccount': 'Đăng ký tài khoản tại?'
+                    'NeedAccount': 'Chưa có tài khoản?'
                 },
                 'en': {
                     'Username': 'Username',
@@ -46,25 +46,26 @@ const app = createApp({
 
 
 function handleLogin() {
-    // console.log("Hello world!");
-    // var clientId = 'c5058ea6-a4ad-44de-ad74-ec502ed357b5';
-    // var redirectUri = 'http%3A%2F%2Flocalhost%3A8080';
-    // var authUrl = 'https://login.microsoftonline.com/b6260bc1-0228-4c6f-9881-15add8fb5383/oauth2/v2.0/authorize';
-    // var responseType = 'code';
-    // var responseMode = 'query';
-    // var scope = 'api%3A%2F%2Fc5058ea6-a4ad-44de-ad74-ec502ed357b5%2FPortal.Access';
+    var clientId = 'c5058ea6-a4ad-44de-ad74-ec502ed357b5';
+    var redirectUri = 'http%3A%2F%2Flocalhost%3A8080';
+    var authUrl = 'https://login.microsoftonline.com/b6260bc1-0228-4c6f-9881-15add8fb5383/oauth2/v2.0/authorize';
+    var responseType = 'code';
+    var responseMode = 'query';
+    var scope = 'api%3A%2F%2Fc5058ea6-a4ad-44de-ad74-ec502ed357b5%2FPortal.Access';
+    var code_challenge_method = 'S256';
+    var code_challenge = 'RSMJjR9NmUtGc7XGsbVPXmiwwTzMwSGb2PdIuhEhhEM';
 
-    // var url = authUrl + 
-    //             '?client_id=' + encodeURIComponent(clientId) + 
-    //             '&response_type=' + encodeURIComponent(responseType) + 
-    //             '&response_mode=' + encodeURIComponent(responseMode) + 
-    //             '&scope=' + encodeURIComponent(scope) + 
-    //             '&redirect_uri=' + encodeURIComponent(redirectUri) + 
-    //             '&state=' + encodeURIComponent(generateRandomState());
-            
-    // console.log("Check url >>>", url)
+    var url = authUrl + 
+                '?client_id=' + clientId + 
+                '&response_type=' + responseType + 
+                '&response_mode=' + responseMode + 
+                '&scope=' + scope + 
+                '&redirect_uri=' + redirectUri + 
+                '&code_challenge=' + code_challenge + 
+                '&code_challenge_method=' + code_challenge_method + 
+                '&state=' + '123456';
 
-    window.location.href = "https://login.microsoftonline.com/b6260bc1-0228-4c6f-9881-15add8fb5383/oauth2/v2.0/authorize?client_id=c5058ea6-a4ad-44de-ad74-ec502ed357b5&response_type=code&response_mode=query&scope=api%3A%2F%2Fc5058ea6-a4ad-44de-ad74-ec502ed357b5%2FPortal.Access&redirect_uri=http%3A%2F%2Flocalhost%3A8080&code_challenge=RSMJjR9NmUtGc7XGsbVPXmiwwTzMwSGb2PdIuhEhhEM&code_challenge_method=S256";
+    window.location.href = url
 
 }
 
